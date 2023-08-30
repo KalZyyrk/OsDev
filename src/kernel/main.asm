701e37bdf0fec8c613a0eb8911ef9419c45955bc
Ulysse Hischke <u.hischke@outlook.fr>
2023-08-30 11:12:31 +0200

    WIP

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,6 +1,7 @@
 org 0x7C00
 bits 16
 
+
 %define ENDL 0x0D, 0x0A
 
 
@@ -9,7 +10,7 @@ start:
 
 
 ;
-; Prints a string to the screen.
+; Prints a string to the screen
 ; Params:
 ;   - ds:si points to string
 ;
@@ -17,42 +18,45 @@ puts:
     ; save registers we will modify
     push si
     push ax
+    push bx
 
 .loop:
-    lodsb           ; loads next character in al
-    or al, al       ; verify if next character is null?
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0e    ; call bios interrupt
-    mov bh, 0
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
-    
+
     jmp .loop
 
 .done:
+    pop bx
     pop ax
-    pop si
+    pop si    
     ret
+    
 
 main:
-
     ; setup data segments
-    mov ax, 0       ; can't write to ds/es directly
+    mov ax, 0           ; can't set ds/es directly
     mov ds, ax
     mov es, ax
-
+    
     ; setup stack
     mov ss, ax
-    mov sp, 0x7C00  ; stack grows downwards from where we are loaded in memory
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
-    ; print message
+    ; print hello world message
     mov si, msg_hello
     call puts
 
     hlt
 
-.halt:
+.halt
     jmp .halt
+
 
 
 msg_hello: db 'Hello world!', ENDL, 0
