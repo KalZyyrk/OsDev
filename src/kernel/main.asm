cb388978266b21e620925ea2ab1e4b4db993380e
Ulysse Hischke <u.hischke@outlook.fr>
2023-08-31 18:16:10 +0200

    WIP

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,4 +1,4 @@
-org 0x7C00
+org 0x0
 bits 16
 
 
@@ -6,60 +6,43 @@ bits 16
 
 
 start:
-    jmp main
+  ; setup data segments
+  mov si, msg_hello
+  call puts
 
+.halt:
+  cli
+  hlt
 
 ;
-; Prints a string to the screen
+; Prints a string to the screen.
 ; Params:
 ;   - ds:si points to string
 ;
 puts:
-    ; save registers we will modify
-    push si
-    push ax
-    push bx
+  ; save registers we will modify
+  push si
+  push ax
+  push bx
 
 .loop:
-    lodsb               ; loads next character in al
-    or al, al           ; verify if next character is null?
-    jz .done
+  lodsb           ; loads next character in al
+  or al, al       ; verify if next character is null?
+  jz .done
 
-    mov ah, 0x0E        ; call bios interrupt
-    mov bh, 0           ; set page number to 0
-    int 0x10
-
-    jmp .loop
+  mov ah, 0x0e    ; call bios interrupt
+  mov bh, 0
+  int 0x10
+    
+  jmp .loop
 
 .done:
-    pop bx
-    pop ax
-    pop si    
-    ret
-    
+  pop bx
+  pop ax
+  pop si
+  ret
 
-main:
-    ; setup data segments
-    mov ax, 0           ; can't set ds/es directly
-    mov ds, ax
-    mov es, ax
-    
-    ; setup stack
-    mov ss, ax
-    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
-
-    ; print hello world message
-    mov si, msg_hello
-    call puts
-
-    hlt
-
-.halt
-    jmp .halt
-
-
-
-msg_hello: db 'Hello world!', ENDL, 0
+msg_hello: db 'Hello world! from KERNEL!', ENDL, 0
 
 
 times 510-($-$$) db 0
